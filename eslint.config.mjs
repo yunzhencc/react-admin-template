6c16a4dde419931eba3ee963d88c93eb5a05dc20
wangxingkang <wang_xingkang@qq.com>
2026-01-15 14:31:21 +0800

    chore: project init

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+  stylistic: {
+    semi: true,
+    indent: 2,
+    quotes: 'single',
+  },
+  rules: {},
+  react: true,
+  typescript: true,
+  ignores: [
+    'tsconfig.json',
+    'tsconfig.app.json',
+    'tsconfig.node.json'
+  ],
+});
