848417bb98aec6d19f948b880bd4884801e837c6
wangxingkang <wang_xingkang@qq.com>
2026-01-19 11:28:25 +0800

    fix: 修复 tailwindcss 无法覆盖 antd 样式问题

--- packages/admin/src/main.tsx
+++ packages/admin/src/main.tsx
@@ -1,4 +1,7 @@
+import { StyleProvider } from '@ant-design/cssinjs';
 import { RouterProvider } from '@tanstack/react-router';
+import { App as AntApp, ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { router } from '@/lib/router';
@@ -15,6 +18,12 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <StyleProvider layer>
+      <ConfigProvider locale={zhCN}>
+        <AntApp>
+          <RouterProvider router={router} />
+        </AntApp>
+      </ConfigProvider>
+    </StyleProvider>
   </StrictMode>,
 );
