1a68c0f7870aafb53c5bfba7d6c2603c5b16b44f
wangxingkang <wang_xingkang@qq.com>
2026-03-10 13:35:49 +0800

    feat: 优化滚动条

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,7 +1,11 @@
+import { ClickScrollPlugin, OverlayScrollbars } from 'overlayscrollbars';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import 'overlayscrollbars/overlayscrollbars.css';
 import './global.css';
+
+OverlayScrollbars.plugin(ClickScrollPlugin);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
